bcb3e4da685ccc749852109f22212df6041f8767
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 22:53:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,16 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(fiction, non-fiction)}
 
+  CLICKBAIT_PATTERNS = [
+   /Won't Believe/i,
+   /Secret/i,
+   /Top [0-9]*/i,
+   /Guess/i
+ ]
 
-end
+ def is_clickbait?
+     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+       errors.add(:title, "must be clickbait")
+     end
+   end
+ end
